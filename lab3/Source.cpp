999678db3d5e3f83ba7e469666d71240281728de
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 03:02:25 +0500

    Добавьте файлы проекта.

---
+++ lab3/Source.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h> 
+#include <omp.h>
+#include <ctime>
+
+int WorkSum(int currI, int n) {
+	int sum = 0;
+	for (int i = currI; i < n; ++i)
+		sum += 1;
+	return sum;
+}
+
+void Task1(int N) {
+	int sum = 0;
+#pragma omp parallel num_threads(2) reduction(+:sum)
+	{
+		int currThread = omp_get_thread_num();
+		//дальше назначать отдельно для каждой нити, если использовать внутри parallel области for, то он будет повторяться k раз, k=numThreads
+		//for(int j=0;j<k;++j)if(currThread=omp_get_thread_num())sum+=Work(j*N/10;++j*N/10);
+		if (currThread == 0) {
+			sum += WorkSum(0, N / 2);
+			printf("[%d]: Sum=%d\n", currThread, sum);
+		}
+		if (currThread == 1) {
+			sum += WorkSum(N / 2, N);
+			printf("[%d]: Sum=%d\n", currThread, sum);
+		}
+	}
+	printf("Sum = %d\n", sum);
+}
+
+int main() {
+	int N = 1000000000, sum = 0, i;
+	const int arrSize = 7;
+	int arrNumbers[arrSize] = { 1,2,5,10,100,1000,N };
+
+	clock_t start, end;
+
+	printf("Task1");
+	for (int i = 0; i < arrSize; ++i) {
+		start = clock();
+		Task1(arrNumbers[i]);
+		end = clock();
+		printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	}
+
+	//добавить массив нитей, если нынешний тред==arrThreads[k]
+//	for (int i = 0; i < arrSize; ++i) {
+//		int sum = 0;
+//		start = clock();
+//#pragma omp parallel num_threads(2) reduction(+:sum)
+//		{
+//			int currThread = omp_get_thread_num();
+//			if (currThread == 0) {
+//				sum += WorkSum(0, arrNumbers[i] / 2);
+//				printf("[%d]: Sum=%d\n", currThread, sum);
+//			}
+//			if (currThread == 1) {
+//				sum += WorkSum(arrNumbers[i] / 2, arrNumbers[i]);
+//				printf("[%d]: Sum=%d\n", currThread, sum);
+//			}
+//		}
+//		printf("Sum = %d\n", sum);
+//		end = clock();
+//		printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+//	}
+	sum = 0;
+	start = clock();
+	for (i = 0; i < N; ++i)
+		sum += 1;
+	end = clock();
+	printf("%d\ntime SERIAL %f", sum, (double)(end - start) / CLOCKS_PER_SEC);
+	return 0;
+}
