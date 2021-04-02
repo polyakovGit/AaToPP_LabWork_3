104c13d8f0d86e8a4c32e7b97393f95c845a26e2
BuildTools <unconfigured@null.spigotmc.org>
2021-04-02 18:08:42 +0500

    задание 5 остался вывод

--- lab3/Source.cpp
+++ lab3/Source.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h> 
 #include <omp.h>
 #include <ctime>
+#include <cmath>
+#include <corecrt_math_defines.h>
 
 int WorkSum(int currI, int n) {
 	int sum = 0;
@@ -41,18 +43,37 @@ void Task2(int N, int k) {
 
 void Task3(int N, int k) {
 	int sum = 0;
-#pragma omp parallel num_threads(k) reduction(+:sum)
+#pragma omp parallel num_threads(k)
 	{
-#pragma omp for
+#pragma omp for reduction(+:sum)
 		for (int i = 0; i < N; ++i)
 			sum += 1;
 		printf("[%d]: Sum=%d\n", omp_get_thread_num(), sum);
 	}
-	printf("Sum=%d\n",sum);
+	printf("Sum=%d\n", sum);
 }
 
 void Task4() {
 	int k = 4, N = 10;
+}
+
+double Task5(long long n, int k) {
+	double sum = 0, x, step = 1. / n;//a=0,b=1 границы интеграла;(b-a)/(double)n в общем виде
+#pragma omp parallel for num_threads(k) reduction (+:sum) private(x)
+	for (long long i = 0; i < n - 1; ++i) {//n-1 для левых
+		x = i * step;//i+0.5 если средних; a+i*step но здесь частный случай для pi 0, сумма не нужна 
+		sum += 4 / (1 + x * x);
+	}
+	return sum / n;
+}
+
+double Task5Serial(long long n) {
+	double sum = 0, x, step = 1. / n;//a=0,b=1 границы интеграла;(b-a)/(double)n в общем виде
+	for (long long i = 0; i < n - 1; ++i) {//n-1 для левых
+		x = i * step;//i+0.5 если средних; a+i*step но здесь частный случай для pi 0, сумма не нужна 
+		sum += 4 / (1 + x * x);
+	}
+	return sum / n;
 }
 
 int main() {
@@ -80,30 +101,49 @@ int main() {
 	//end = clock();
 	//printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
 
-	const int  arrThreadsSize = 4;
-	int arrThreads[arrThreadsSize] = { 2,4,8,16 };
-	printf("Task2\n");
-	for (int k = 0; k < arrThreadsSize; ++k) {
-		for (int i = 0; i < arrNumbersSize; ++i) {
-			start = clock();
-			Task2(arrNumbers[i], arrThreads[k]);
-			end = clock();
-			printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-		}
-	}
+	//const int  arrThreadsSize = 4;
+	//int arrThreads[arrThreadsSize] = { 2,4,8,16 };
+	//printf("Task2\n");
+	//for (int k = 0; k < arrThreadsSize; ++k) {
+	//	for (int i = 0; i < arrNumbersSize; ++i) {
+	//		start = clock();
+	//		Task2(arrNumbers[i], arrThreads[k]);
+	//		end = clock();
+	//		printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	//	}
+	//}
 
-	printf("Task3");
-	for (int k = 0; k < arrThreadsSize; ++k) {
-		for (int i = 0; i < arrNumbersSize; ++i) {
-			start = clock();
-			Task3(arrNumbers[i], arrThreads[k]);
-			end = clock();
-		}
-		printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-	}
+	//printf("Task3\n");
+	//for (int k = 0; k < arrThreadsSize; ++k) {
+	//	for (int i = 0; i < arrNumbersSize; ++i) {
+	//		start = clock();
+	//		Task3(arrNumbers[i], arrThreads[k]);
+	//		end = clock();
+	//	}
+	//	printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	//}
 
-	printf("Task4");
-	Task4();
+	//printf("Task4\n");
+	//Task4();
+
+	printf("Task5\n");
+	long long n = 2;
+	int i = 0;
+	double eps = 0.001;
+	for (int k = 1; k < 20; ++k) {
+		do {
+			start = clock();
+			double myPi = Task5(n, k);
+			end = clock();
+			if (fabs(M_PI - myPi) > eps)n *= 2;
+			else {
+				//printf("ITERATION %d\nN=%lld\neps   %.10f\n FABS %.10f\n   PI %.10f\nvalue %.10f\ntime %f\n\n",i++, n, eps,
+				//	fabs(M_PI - myPi), M_PI, myPi, (double)(end - start) / CLOCKS_PER_SEC);
+				printf("%.10f\nk= %d\n%.10f\n\n", myPi,k, Task5Serial(n));//проверить с разным количеством нитей значение pi
+				eps *= 0.1;
+			}
+		} while ((double)(end - start) / CLOCKS_PER_SEC < 120);
+	}
 
 	//добавить массив нитей, если нынешний тред==arrThreads[k]
 //	for (int i = 0; i < arrSize; ++i) {
@@ -125,6 +165,8 @@ int main() {
 //		end = clock();
 //		printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
 //	}
+
+
 	sum = 0;
 	start = clock();
 	for (int i = 0; i < N; ++i)
