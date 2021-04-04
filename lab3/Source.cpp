363e61e2d36591dd63a18fb3d0a65a4ce32f8edf
BuildTools <unconfigured@null.spigotmc.org>
2021-04-04 22:25:32 +0500

    Все выполнено

--- lab3/Source.cpp
+++ lab3/Source.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <corecrt_math_defines.h>
 #include <iostream>
+#include <fstream>
 
 int WorkSum(int currI, int n) {
 	int sum = 0;
@@ -44,9 +45,9 @@ void Task2(int N, int k) {
 
 void Task3(int N, int k) {
 	int sum = 0;
-#pragma omp parallel num_threads(k)
+#pragma omp parallel num_threads(k) reduction(+:sum)
 	{
-#pragma omp for reduction(+:sum)
+#pragma omp for 
 		for (int i = 0; i < N; ++i)
 			sum += 1;
 		printf("[%d]: Sum=%d\n", omp_get_thread_num(), sum);
@@ -56,6 +57,96 @@ void Task3(int N, int k) {
 
 void Task4() {
 	int k = 4, N = 10;
+	int sum = 0;
+	printf("schedule static\n");
+#pragma omp parallel num_threads(k) reduction(+:sum)
+	{
+#pragma omp for schedule(static)
+		for (int i = 1; i <= N; ++i) {
+			sum += 1;
+			printf("Iteraion [%d] thread [%d]\n", i, omp_get_thread_num());
+		}
+		printf("[%d]: Sum=%d\n", omp_get_thread_num(), sum);
+	}
+	printf("Sum=%d\n", sum);
+
+	sum = 0;
+	printf("schedule static 1\n");
+#pragma omp parallel num_threads(k) reduction(+:sum)
+	{
+#pragma omp for schedule(static,1)
+		for (int i = 1; i <= N; ++i) {
+			sum += 1;
+			printf("Iteraion [%d] thread [%d]\n", i, omp_get_thread_num());
+		}
+		printf("[%d]: Sum=%d\n", omp_get_thread_num(), sum);
+	}
+	printf("Sum=%d\n", sum);
+
+	sum = 0;
+	printf("schedule static 2\n");
+#pragma omp parallel num_threads(k) reduction(+:sum)
+	{
+#pragma omp for schedule(static,2)
+		for (int i = 1; i <= N; ++i) {
+			sum += 1;
+			printf("Iteraion [%d] thread [%d]\n", i, omp_get_thread_num());
+		}
+		printf("[%d]: Sum=%d\n", omp_get_thread_num(), sum);
+	}
+	printf("Sum=%d\n", sum);
+
+	sum = 0;
+	printf("schedule dynamic\n");
+#pragma omp parallel num_threads(k) reduction(+:sum)
+	{
+#pragma omp for schedule(dynamic)
+		for (int i = 1; i <= N; ++i) {
+			sum += 1;
+			printf("Iteraion [%d] thread [%d]\n", i, omp_get_thread_num());
+		}
+		printf("[%d]: Sum=%d\n", omp_get_thread_num(), sum);
+	}
+	printf("Sum=%d\n", sum);
+
+	sum = 0;
+	printf("schedule dynamic 2\n");
+#pragma omp parallel num_threads(k) reduction(+:sum)
+	{
+#pragma omp for schedule(dynamic,2)
+		for (int i = 1; i <= N; ++i) {
+			sum += 1;
+			printf("Iteraion [%d] thread [%d]\n", i, omp_get_thread_num());
+		}
+		printf("[%d]: Sum=%d\n", omp_get_thread_num(), sum);
+	}
+	printf("Sum=%d\n", sum);
+
+	sum = 0;
+	printf("schedule guided\n");
+#pragma omp parallel num_threads(k) reduction(+:sum)
+	{
+#pragma omp for schedule(guided)
+		for (int i = 1; i <= N; ++i) {
+			sum += 1;
+			printf("Iteraion [%d] thread [%d]\n", i, omp_get_thread_num());
+		}
+		printf("[%d]: Sum=%d\n", omp_get_thread_num(), sum);
+	}
+	printf("Sum=%d\n", sum);
+
+	sum = 0;
+	printf("schedule guided 2\n");
+#pragma omp parallel num_threads(k) reduction(+:sum)
+	{
+#pragma omp for schedule(guided,2)
+		for (int i = 1; i <= N; ++i) {
+			sum += 1;
+			printf("Iteraion [%d] thread [%d]\n", i, omp_get_thread_num());
+		}
+		printf("[%d]: Sum=%d\n", omp_get_thread_num(), sum);
+	}
+	printf("Sum=%d\n", sum);
 }
 
 double Task5(long long n, int k) {
@@ -68,14 +159,14 @@ double Task5(long long n, int k) {
 	return sum / n;
 }
 
-double Task5Serial(long long n) {
-	double sum = 0, x, step = 1. / n;//a=0,b=1 границы интеграла;(b-a)/(double)n в общем виде
-	for (long long i = 0; i < n - 1; ++i) {//n-1 для левых
-		x = i * step;//i+0.5 если средних; a+i*step но здесь частный случай для pi 0, сумма не нужна 
-		sum += 4 / (1 + x * x);
-	}
-	return sum / n;
-}
+//double Task5Serial(long long n) {
+//	double sum = 0, x, step = 1. / n;//a=0,b=1 границы интеграла;(b-a)/(double)n в общем виде
+//	for (long long i = 0; i < n - 1; ++i) {//n-1 для левых
+//		x = i * step;//i+0.5 если средних; a+i*step но здесь частный случай для pi 0, сумма не нужна 
+//		sum += 4 / (1 + x * x);
+//	}
+//	return sum / n;
+//}
 
 int main() {
 
@@ -85,58 +176,56 @@ int main() {
 
 	clock_t start, end;
 
-	//printf("Task1\n");
-	//for (int i = 0; i < arrNumbersSize; ++i) {
-	//	start = clock();
-	//	Task1(arrNumbers[i]);
-	//	end = clock();
-	//	printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-	//}
+	printf("Task1\n");
+	for (int i = 0; i < arrNumbersSize; ++i) {
+		start = clock();
+		Task1(arrNumbers[i]);
+		end = clock();
+		printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	}
 
-	//start = clock();
-	//Task2(N, 4);
-	//end = clock();
-	//printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-	//start = clock();
-	//Task3(N, 4);
-	//end = clock();
-	//printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	const int  arrThreadsSize = 4;
+	int arrThreads[arrThreadsSize] = { 2,4,8,16 };
 
-	//const int  arrThreadsSize = 4;
-	//int arrThreads[arrThreadsSize] = { 2,4,8,16 };
-	//printf("Task2\n");
-	//for (int k = 0; k < arrThreadsSize; ++k) {
-	//	for (int i = 0; i < arrNumbersSize; ++i) {
-	//		start = clock();
-	//		Task2(arrNumbers[i], arrThreads[k]);
-	//		end = clock();
-	//		printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-	//	}
-	//}
+	printf("Task2\n");
+	for (int k = 0; k < arrThreadsSize; ++k) {
+		for (int i = 0; i < arrNumbersSize; ++i) {
+			start = clock();
+			Task2(arrNumbers[i], arrThreads[k]);
+			end = clock();
+			printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+		}
+	}
 
-	//printf("Task3\n");
-	//for (int k = 0; k < arrThreadsSize; ++k) {
-	//	for (int i = 0; i < arrNumbersSize; ++i) {
-	//		start = clock();
-	//		Task3(arrNumbers[i], arrThreads[k]);
-	//		end = clock();
-	//	}
-	//	printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-	//}
+	printf("Task3\n");
+	for (int k = 0; k < arrThreadsSize; ++k) {
+		for (int i = 0; i < arrNumbersSize; ++i) {
+			start = clock();
+			Task3(arrNumbers[i], arrThreads[k]);
+			end = clock();
+		}
+		printf("time %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	}
 
-	//printf("Task4\n");
-	//Task4();
+	printf("Task4\n");
+	Task4();
 
 	printf("Task5\n");
+	std::ofstream OutputData;
+	OutputData.open("OutputData.csv");
 	long long n = 2;
 	int i = 0;
 	double eps = 0.001, time;
 	printf("\t");
-	for (int k = 1; k <= 4; ++k)
+	OutputData << ';';
+	for (int k = 1; k <= 20; ++k) {
 		printf("\t%d\t", k);
+		OutputData << ';' << k;
+	}
 	do {
 		printf("\n%.10f\t", eps);
-		for (int k = 1; k <= 4;) {
+		OutputData << '\n' << eps << ';';
+		for (int k = 1; k <= 20;) {
 			start = clock();
 			double myPi = Task5(n, k);
 			end = clock();
@@ -144,18 +233,19 @@ int main() {
 			if (fabs(M_PI - myPi) > eps)n *= 2;
 			else {
 				printf("%.10f\t", time);
+				OutputData << time << ';';
 				if (time > 120)break;
 				++k;
 			}
 		}
 		eps *= 0.1;
-	} while (time < 120);
+	} while (time <= 120);
 
-	sum = 0;
-	start = clock();
-	for (int i = 0; i < N; ++i)
-		sum += 1;
-	end = clock();
-	printf("%d\ntime SERIAL %f", sum, (double)(end - start) / CLOCKS_PER_SEC);
+	//sum = 0;
+	//start = clock();
+	//for (int i = 0; i < N; ++i)
+	//	sum += 1;
+	//end = clock();
+	//printf("%d\ntime SERIAL %f", sum, (double)(end - start) / CLOCKS_PER_SEC);
 	return 0;
 }
